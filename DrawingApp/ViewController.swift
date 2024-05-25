27c92bda408d14204d2654b21d8c58a318ef8674
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-05-25 12:40:32 -0700

    Commit

--- DrawingApp/ViewController.swift
+++ DrawingApp/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import PencilKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var canvasView: PKCanvasView!
+    var toolPicker: PKToolPicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        toolPicker = PKToolPicker()
+                toolPicker.setVisible(true, forFirstResponder: canvasView)
+                toolPicker.addObserver(canvasView)
+                canvasView.becomeFirstResponder()
     }
 
-
+    @IBAction func btnClearTapped(_ sender: UIBarButtonItem) {
+        canvasView.drawing = PKDrawing()
+    }
+    
 }
 
